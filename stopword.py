62d5b7269abc1c1ec4f9e66bd0757d96df776701
PraveenSubramaniyam <praveenatceg@gmail.com>
2016-04-21 19:13:00 -0700

    Changed The sentence

--- stopword.py
+++ stopword.py
@@ -1,6 +1,6 @@
 from nltk.corpus import stopwords
 
-word = "hello how are you"
+word = "Tiger is national animal teh"
 word_list = word.split();
 print word_list
 filtered_word_list = word_list[:] #make a copy of the word_list
