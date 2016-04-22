d40bc74f3e6c39039a1831f7dfb79f7bbc57c4ec
PraveenSubramaniyam <praveenatceg@gmail.com>
2016-04-21 19:09:15 -0700

    Added Code for Removing stop word from comments

---
+++ stopword.py
@@ -0,0 +1,11 @@
+from nltk.corpus import stopwords
+
+word = "hello how are you"
+word_list = word.split();
+print word_list
+filtered_word_list = word_list[:] #make a copy of the word_list
+for word in word_list: # iterate over word_list
+  if word in stopwords.words('english'): 
+    filtered_word_list.remove(word)
+    
+print filtered_word_list
