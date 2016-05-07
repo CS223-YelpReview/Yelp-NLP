4c11da2dcbe3581b85c45e55d81be9254b985eed
PraveenSubramaniyam <praveenatceg@gmail.com>
2016-05-07 16:24:19 -0700

    Naive Bayes Implementation

--- ExtractReview.py
+++ ExtractReview.py
@@ -4,131 +4,149 @@ import csv
 from nltk.corpus import stopwords
 import re
 
+class naiveBayes:
+    stopset = set(stopwords.words('english'))
 
-posWordDict = dict()
-posWordProb = dict()
-negWordDict = dict()
-negWordProb = dict()
-allWordDict = dict()
-probPositive = 0
-probNegative = 0
-countReviews = 0
-countPosReviews = 0
-countNegReviews= 0
-    
-stopset = set(stopwords.words('english'))
-
-def stringTok(string):
-    word_list = string.split()
-    filtered_word_list = word_list[:] #make a copy of the word_list
-    for word in word_list: # iterate over word_list
-        if word in stopwords.words('english'): 
-            filtered_word_list.remove(word)
+    def __init__(self):
+        self.posWordDict = dict()
+        self.posWordProb = dict()
+        self.negWordDict = dict()
+        self.negWordProb = dict()
+        self.allWordDict = dict()
+        self.probPositive = 0
+        self.probNegative = 0
+        self.countReviews = 0
+        self.countPosReviews = 0
+        self.countNegReviews= 0
+        
+    def stringTok(self,string):
+        word_list = string.split()
+        filtered_word_list = word_list[:] #make a copy of the word_list
+        for word in word_list: # iterate over word_list
+            if word in stopwords.words('english'): 
+                filtered_word_list.remove(word)
             
-        filteredString = ' '.join(filtered_word_list)
-        filteredString = re.sub('[^A-Za-z ]+', '', filteredString)
-    return filteredString
+            filteredString = ' '.join(filtered_word_list)
+            filteredString = re.sub('[^A-Za-z ]+', '', filteredString)
+        return filteredString
 
 
 
-def outputReviewsCSV(jsonFile, outputCSV, businessId, dictKeys):
-    csvReviewFile = open(outputCSV, 'w')
-    csvFileWriter = csv.writer(csvReviewFile)
-    with open(jsonFile,'rb') as jsonFile:
-        for line in jsonFile:
-            json_data = ujson.loads(line)
+    def outputReviewsCSV(self,jsonFile, outputCSV, businessId, dictKeys):
+        csvReviewFile = open(outputCSV, 'w')
+        csvFileWriter = csv.writer(csvReviewFile)
+        with open(jsonFile,'rb') as jsonFile:
+            for line in jsonFile:
+                json_data = ujson.loads(line)
+                currentData = []
+                if json_data['business_id'] in businessId:
+                    for key in dictKeys:
+                        if isinstance(json_data[key],basestring):
+                            word_list = json_data[key].encode('ascii','ignore').lower().split()
+                            filtered_word_list = word_list[:] #make a copy of the word_list
+                            for word in word_list: # iterate over word_list
+                                if word in stopwords.words('english'): 
+                                    filtered_word_list.remove(word)
+                                    
+                                filteredString = ' '.join(filtered_word_list)
+                                filteredString = re.sub('[^A-Za-z ]+', '', filteredString)
+                            currentData.append(filteredString)
+                        else:
+                            currentData.append(json_data[key])
+                    
+                    csvFileWriter.writerow(currentData)
+        csvReviewFile.close()    
+    
+
+    def formProbability(self,csvFile):
+        with open(csvFile, "rb") as f:
+            reader = csv.reader(f, delimiter="\t")
+            for lines in reader:
+                self.countReviews += 1
+                line = lines[0].split(',')
+                wordList = line[0].split()
+                if(int(line[1])<3):
+                    self.countNegReviews += 1
+                    for word in wordList:
+                        if self.negWordDict.has_key(word):
+                            self.negWordDict[word] += 1
+                        else:
+                            self.negWordDict[word] = 1
+                else:
+                    self.countPosReviews += 1
+                    for word in wordList:
+                        if self.posWordDict.has_key(word):
+                            self.posWordDict[word] += 1
+                        else:
+                            self.posWordDict[word] = 1
+            
+        f.close()
+    
+
+    def naiveBayes(self):
+        entireVoc =  set(self.negWordDict.keys())
+        entireVoc.update(self.posWordDict.keys())
+        totalCount = len(entireVoc)
+        negWordCount = len(self.negWordDict)
+        posWordCount = len(self.posWordDict)
+    
+        for key,value in self.negWordDict.iteritems():
+            self.negWordProb[key] = (1 + value) / (negWordCount + totalCount)
+        
+        for key,value in self.posWordDict.iteritems():
+            self.posWordProb[key] = (1 + value) / (posWordCount + totalCount)
+        print "Negative Probablity:\n", self.negWordProb
+        print "\n"
+        print "Positive Probablity:\n", self.posWordProb
+    
+
+
+    def findProbablity(self,review_input):
+        posProb = self.countPosReviews / self.countReviews
+        negProb = self.countNegReviews / self.countReviews
+        posProbWord = posProb
+        negProbWord = negProb
+        filteredString = self.stringTok(review_input)
+        for word in filteredString:
+            if word in self.posWordProb:
+                posProbWord = posProbWord * self.posWordProb[word]
+            
+            if word in self.negWordProb:
+                negProbWord = negProbWord * self.negWordProb[word]
+        
+        print "Positive Prob word: ", posProbWord
+        print "Positive Neg word: ", negProbWord
+        if(posProbWord > negProbWord):
+            print "Positive Review"
+        else:
+            print "Negative Review"
+        
+    
+    def appendDataToCSV(self,jsonFileToRead):
+    #def appendDataToCSV(csvFile,jsonFileToRead,dataTitles):
+        #csvReviewFile = open(csvFile, 'a+')
+        #csvFileWriter = csv.writer(csvReviewFile)
+        #csvFileWriter.writerow(dataTitles)
+        with open(jsonFileToRead, 'rb') as jsonFile:
             currentData = []
-            if json_data['business_id'] in businessId:
-                for key in dictKeys:
-                    if isinstance(json_data[key],basestring):
-                        word_list = json_data[key].encode('ascii','ignore').lower().split()
-                        filtered_word_list = word_list[:] #make a copy of the word_list
-                        for word in word_list: # iterate over word_list
-                            if word in stopwords.words('english'): 
-                                filtered_word_list.remove(word)
-                                
-                            filteredString = ' '.join(filtered_word_list)
-                            filteredString = re.sub('[^A-Za-z ]+', '', filteredString)
-                        currentData.append(filteredString)
-                    else:
-                        currentData.append(json_data[key])
-                   
-                csvFileWriter.writerow(currentData)
-    csvReviewFile.close()    
-    
-
-def formProbability(csvFile):
-    global countReviews
-    global countNegReviews
-    global countPosReviews
-    with open(csvFile, "rb") as f:
-        reader = csv.reader(f, delimiter="\t")
-        for lines in reader:
-            countReviews += 1
-            line = lines[0].split(',')
-            wordList = line[0].split()
-            if(int(line[1])<3):
-                countNegReviews += 1
-                for word in wordList:
-                    if negWordDict.has_key(word):
-                        negWordDict[word] += 1
-                    else:
-                        negWordDict[word] = 1
-            else:
-                countPosReviews += 1
-                for word in wordList:
-                    if posWordDict.has_key(word):
-                        posWordDict[word] += 1
-                    else:
-                        posWordDict[word] = 1
-            
-    f.close()
-    
-
-def naiveBayes():
-    entireVoc =  set(negWordDict.keys())
-    entireVoc.update(posWordDict.keys())
-    totalCount = len(entireVoc)
-    negWordCount = len(negWordDict)
-    posWordCount = len(posWordDict)
-    
-   
-    for key,value in negWordDict.iteritems():
-        negWordProb[key] = (1 + value) / (negWordCount + totalCount)
-        
-    for key,value in posWordDict.iteritems():
-        posWordProb[key] = (1 + value) / (posWordCount + totalCount)
-    
-    print "Negative Probablity:\n", negWordProb
-    print "\n"
-    print "Positive Probablity:\n", posWordProb
-    
-outputReviewsCSV('F:\Courses\Bio Informatics\Project\yelp_dataset_challenge_academic_dataset\yelp_academic_dataset_review.json','F:\Courses\Bio Informatics\Project\yelp_dataset_challenge_academic_dataset\yelp_Review_CSV.csv',['UsFtqoBl7naz8AVUBZMjQQ'],['text','stars'])
-formProbability('F:\Courses\Bio Informatics\Project\yelp_dataset_challenge_academic_dataset\yelp_Review_CSV.csv')
-naiveBayes()
-
-def findProbablity(review_input):
-    posProb = countPosReviews / countReviews
-    negProb = countNegReviews / countReviews
-    posProbWord = posProb
-    negProbWord = negProb
-    filteredString = stringTok(review_input)
-    for word in filteredString:
-        if word in posWordProb:
-            posProbWord = posProbWord * posWordProb[word]
-            
-        if word in negWordProb:
-            negProbWord = negProbWord * negWordProb[word]
-        
-    print "Positive Prob word: ", posProbWord
-    print "Positive Neg word: ", negProbWord
-    if(posProbWord > negProbWord):
-        print "Positive Review"
-    else:
-        print "Negative Review"
-        
-    
-    
-
+            for line in jsonFile:
+                json_data = ujson.loads(line)
+                category = ''.join(json_data['categories'])
+                if "food" in category.lower():
+                    currentData.append(json_data['business_id'])
+                    #currentData.append(json_data['categories'])
+                    #csvFileWriter.writerow(currentData)
+                    #json_data['business_id']
+        #csvReviewFile.close()
+        return currentData
+ 
+naiveBayesObj = naiveBayes()
+businessId = naiveBayesObj.appendDataToCSV('F:\Courses\Bio Informatics\Project\dataset\yelp_academic_dataset_business.json')
+print len(businessId)
+businessId = businessId[:100]
+print businessId
+naiveBayesObj.outputReviewsCSV('F:\Courses\Bio Informatics\Project\dataset\yelp_academic_dataset_review.json','F:\Courses\Bio Informatics\Project\dataset\yelp_Review_CSV.csv',businessId,['text','stars'])
+naiveBayesObj.formProbability('F:\Courses\Bio Informatics\Project\dataset\yelp_Review_CSV.csv')
+naiveBayesObj.naiveBayes()
 var = raw_input("Enter the Review: ")
-findProbablity (var)
+naiveBayesObj.findProbablity (var)
